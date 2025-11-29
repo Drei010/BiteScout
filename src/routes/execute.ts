8286a9bb4f76e24d9b38a1ff997877e0a53f6254
Andrei <8700andreihidalgo@gmail.com>
2025-11-30 00:16:34 +0800

    added foursquare api and package.json

--- src/routes/execute.ts
+++ src/routes/execute.ts
@@ -1,35 +1,34 @@
 import type { Express } from "express";
 import convertToJSON from "../services/llm.service.js";
+import callPlaceSearch from "../services/foursquare.service.js";
 import validation from "../utils/validation.js";
 
 const { validateInputParams, authenticateUser } = validation;
 
-
 export default function setupRoutes(app: Express) {
-app.get("/", (req, res) => {
-  res.send("Restaurant Finder AI Server is running");
-});
+  app.get("/", (req, res) => {
+    res.send("Restaurant Finder AI Server is running");
+  });
 
-app.get(
-  "/api/execute",
-  validateInputParams,
-  authenticateUser,
-  async (req, res) => {
-    try {
-      const message = req.query.message as string;
-      const code = req.query.code as string;
+  app.get(
+    "/api/execute",
+    validateInputParams,
+    authenticateUser,
+    async (req, res) => {
+      try {
+        const message = req.query.message as string;
+        const code = req.query.code as string;
 
-      console.log("mesage", message);
-      console.log("code", code);
+        console.log("mesage", message);
+        console.log("code", code);
 
-      const data = await convertToJSON(message);
-
-      res.json(data);
-    } catch (error) {
-      console.error("Error fetching LLM response:", error);
-      res.status(400).json(error);
+        const data = await convertToJSON(message);
+        // console.log("data", callPlaceSearch(data));
+        res.json(data);
+      } catch (error) {
+        console.error("Error fetching LLM response:", error);
+        res.status(500).json(error);
+      }
     }
-  }
-);
+  );
 }
-
